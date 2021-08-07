08af26326b8ed257c543d49a2a43e1b8920dc784
PonasKovas <mykolas.peteraitis@gmail.com>
2021-08-07 18:19:47 +0300

    callback based plugin registering

--- bws/src/plugins.rs
+++ bws/src/plugins.rs
@@ -3,21 +3,51 @@ use async_ffi::FfiFuture;
 use libloading::{Library, Symbol};
 use log::{error, info};
 use semver::{Version, VersionReq};
-use std::{collections::HashMap, path::Path};
+use sha2::digest::generic_array::transmute;
+use std::mem::swap;
+use std::path::PathBuf;
+use std::{
+    collections::HashMap,
+    ffi::{CStr, CString},
+    path::Path,
+    sync::Arc,
+};
 use tokio::fs;
 
-const ABI_VERSION: u32 = async_ffi::ABI_VERSION << 16 | crate::ABI_VERSION;
+use crate::plugins;
 
-pub struct Plugins {}
+const ABI_VERSION: u64 = ((async_ffi::ABI_VERSION as u64) << 32) | crate::ABI_VERSION as u64;
+
+pub type Plugins = HashMap<CString, Plugin>;
+pub struct Plugin {
+    pub version: CString,
+    pub provided_by: PathBuf,
+    pub dependencies: Vec<(CString, CString)>,
+    pub callbacks: Callbacks,
+}
+
+#[derive(Default)]
+pub struct Callbacks {
+    init: Option<Callback<extern "C" fn(), extern "C" fn() -> FfiFuture<()>>>,
+    /// Other callbacks the plugin may register, that may be used by other plugins
+    /// It will be up to them transmute the pointer to a correct function pointer
+    other: HashMap<String, Callback<*const (), *const ()>>,
+}
+
+#[repr(C)]
+pub enum Callback<S, A> {
+    Sync(S),
+    Async(A),
+}
 
 pub async fn load_plugins() -> Result<Plugins> {
-    let mut plugins = HashMap::new();
+    let mut plugins = Plugins::new();
 
     let mut read_dir = fs::read_dir("plugins").await?;
     while let Some(path) = read_dir.next_entry().await? {
         let path = path.path().canonicalize()?;
 
-        // ignore directories and files starting with .
+        // ignore directories
         if path.is_dir() {
             continue;
         }
@@ -25,46 +55,59 @@ pub async fn load_plugins() -> Result<Plugins> {
             // also skip files with invalid unicode in their names
             None => continue,
             Some(path) => {
+                // skip hidden files
                 if path.starts_with('.') {
                     continue;
                 }
             }
         }
 
-        match unsafe { load_plugin(&path).await } {
-            Ok((name, version, lib)) => {
-                plugins.insert(name, (version, lib));
-            }
-            Err(e) => {
-                error!("Error loading {:?}: {:?}", path.file_name().unwrap(), e);
-            }
+        if let Err(e) = unsafe { load_library(&mut plugins, &path).await } {
+            error!("Error loading {:?}: {:?}", path.file_name().unwrap(), e);
         }
     }
 
-    // ok, the libraries have been loaded, now time to check their dependencies, and if met, initialize them
-    for plugin in &plugins {
-        match unsafe { dependencies_matched(plugin, &plugins) } {
+    // check the plugins and remove any that have unsatisfied dependencies
+    let keys_to_remove = plugins
+        .keys()
+        .filter(|k| match check_dependencies(k, &plugins) {
             Ok(true) => {
-                // everything alright, now we can finally initialize and prepare them for usage
+                info!(
+                    "Plugin {:?} loaded. (Provided by {:?})",
+                    k, plugins[*k].provided_by
+                );
+                false
             }
             Ok(false) => {
-                // :/
-                // sad, but gotta move on
+                error!(
+                    "Plugin {:?} could not be loaded, because it's dependencies weren't satisfied. (Provided by {:?})",
+                    k, plugins[*k].provided_by
+                );
+                true
             }
             Err(e) => {
-                error!("Error reading dependencies of plugin {}: {:?}", plugin.0, e);
-                // now this is truly sad! but still have to move on...
+                error!(
+                    "Error reading dependencies of plugin {:?} (Provided by {:?}): {:?}",
+                    k,
+                    plugins[*k].provided_by,
+                    e
+                );
+                true
             }
-        }
+        })
+        .cloned()
+        .collect::<Vec<_>>();
+    for key in keys_to_remove {
+        plugins.remove(&key);
     }
 
-    todo!()
+    Ok(plugins)
 }
 
-async unsafe fn load_plugin(path: impl AsRef<Path>) -> Result<(String, Version, Library)> {
-    let lib = Library::new(path.as_ref())?;
+async unsafe fn load_library(plugins: &mut Plugins, path: impl AsRef<Path>) -> Result<()> {
+    let lib = Arc::new(Library::new(path.as_ref())?);
 
-    let abi_version: Symbol<*const u32> = lib.get(b"ABI_VERSION")?;
+    let abi_version: Symbol<*const u64> = lib.get(b"ABI_VERSION")?;
 
     if **abi_version != ABI_VERSION {
         bail!(
@@ -74,52 +117,136 @@ async unsafe fn load_plugin(path: impl AsRef<Path>) -> Result<(String, Version, 
         );
     }
 
-    let plugin_name: Symbol<*const &[u8]> = lib.get(b"PLUGIN_NAME")?;
-    let plugin_version: Symbol<*const &[u8]> = lib.get(b"PLUGIN_VERSION")?;
+    // register the plugins
 
-    Ok((
-        std::str::from_utf8(**plugin_name)
-            .context("reading PLUGIN_NAME")?
-            .to_owned(),
-        Version::parse(std::str::from_utf8(**plugin_version).context("reading PLUGIN_VERSION")?)
-            .context("parsing PLUGIN_VERSION")?,
-        lib,
-    ))
-}
+    // Vec<((plugin_name, version), dependencies, callbacks)>
+    #[allow(non_upper_case_globals)]
+    static mut to_register: Vec<((CString, CString), Vec<(CString, CString)>, Callbacks)> =
+        Vec::new();
 
-unsafe fn dependencies_matched(
-    plugin: (&String, &(Version, Library)),
-    plugins: &HashMap<String, (Version, Library)>,
-) -> Result<bool> {
-    let dependencies: Symbol<*const &[(&[u8], &[u8])]> =
-        (plugin.1).1.get(b"PLUGIN_DEPENDENCIES")?;
+    type RegisterPluginType =
+        unsafe extern "C" fn(*const i8, *const i8, usize, *const *const i8) -> RegisterCallbackType;
+    unsafe extern "C" fn register_plugin(
+        name: *const i8,
+        version: *const i8,
+        dependencies_n: usize,
+        dependencies: *const *const i8,
+    ) -> RegisterCallbackType {
+        to_register.push((
+            (
+                CStr::from_ptr(name).to_owned(),
+                CStr::from_ptr(version).to_owned(),
+            ),
+            {
+                let mut deps = Vec::new();
+                for i in 0..dependencies_n {
+                    deps.push((
+                        CStr::from_ptr(*dependencies.add(i * 2)).to_owned(),
+                        CStr::from_ptr(*dependencies.add(i * 2 + 1)).to_owned(),
+                    ));
+                }
+                deps
+            },
+            Default::default(),
+        ));
 
-    for dependency in **dependencies {
-        let dependency_name = std::str::from_utf8(dependency.0)?;
-        let dependency_req = std::str::from_utf8(dependency.1)?;
+        register_callback
+    }
 
-        let dependency_req = semver::VersionReq::parse(dependency_req)
-            .context("error parsing version requirement")?;
+    type RegisterCallbackType = unsafe extern "C" fn(*const i8, *const (), bool);
+    unsafe extern "C" fn register_callback(
+        callback_name: *const i8,
+        fn_ptr: *const (),
+        is_async: bool,
+    ) {
+        let plugin = to_register.last_mut().unwrap();
 
-        match plugins.get(dependency_name) {
-            Some(dep_plugin) => {
-                if !dependency_req.matches(&dep_plugin.0) {
-                    error!(
-                        "Plugin's \"{}\" dependency {} {} was not met. {} {} is present, but does not match the {} version requirement. Skipping.",
-                        plugin.0, dependency_name, dependency_req, dependency_name, dep_plugin.0, dependency_req
-                    );
-                    return Ok(false);
+        let callback_name = CStr::from_ptr(callback_name);
+        let callback_name = match callback_name.to_str() {
+            Ok(s) => s,
+            Err(_) => {
+                error!(
+                    "Plugin {:?} tried to register a callback with a name that is not valid unicode: {:?}",
+                    plugin.0.0,
+                    callback_name
+                );
+                return;
+            }
+        };
+
+        match callback_name {
+            "init" => {
+                if is_async {
+                    plugin.2.init = Some(Callback::Async(transmute(fn_ptr)));
+                } else {
+                    plugin.2.init = Some(Callback::Sync(transmute(fn_ptr)));
                 }
             }
-            None => {
-                error!(
-                    "Plugin's \"{}\" dependency {} {} was not met. Skipping.",
-                    plugin.0, dependency_name, dependency_req
-                );
-                return Ok(false);
+            other => {
+                if is_async {
+                    plugin
+                        .2
+                        .other
+                        .insert(other.to_owned(), Callback::Async(fn_ptr));
+                } else {
+                    plugin
+                        .2
+                        .other
+                        .insert(other.to_owned(), Callback::Sync(fn_ptr));
+                }
             }
         }
     }
 
-    Ok(true)
+    let plugin_registrator: Symbol<unsafe extern "C" fn(RegisterPluginType)> =
+        lib.get(b"register_plugin")?;
+
+    (*plugin_registrator)(register_plugin);
+
+    let mut to_register_non_static = Vec::new();
+    swap(&mut to_register, &mut to_register_non_static);
+    for plugin in to_register_non_static {
+        plugins.insert(
+            plugin.0 .0,
+            Plugin {
+                version: plugin.0 .1,
+                provided_by: path.as_ref().to_path_buf(),
+                dependencies: plugin.1,
+                callbacks: plugin.2,
+            },
+        );
+    }
+
+    Ok(())
+}
+
+fn check_dependencies(plugin_name: &CString, plugins: &Plugins) -> Result<bool> {
+    let mut result = true;
+
+    let plugin = &plugins[plugin_name];
+    for dependency in &plugin.dependencies {
+        let dependency_req = VersionReq::parse(dependency.1.to_str()?)
+            .context("error parsing version requirement")?;
+
+        match plugins.get(&dependency.0) {
+            Some(dep_plugin) => {
+                let dep_version = Version::parse(dep_plugin.version.to_str()?)?;
+                if !dependency_req.matches(&dep_version) {
+                    error!(
+                        "Plugin {:?} dependency {:?} {} was not met. {:?} {} is present, but does not match the {} version requirement.",
+                        plugin_name, dependency.0, dependency_req, dependency.0, dep_version, dependency_req
+                    );
+                    result = false;
+                }
+            }
+            None => {
+                error!(
+                    "Plugin {:?} dependency {:?} {} was not met.",
+                    plugin_name, dependency.0, dependency_req
+                );
+                result = false;
+            }
+        }
+    }
+    Ok(result)
 }
