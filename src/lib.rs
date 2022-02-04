14a1de1dd1b780d9055614561f69ea5bebcf835b
PonasKovas <mykolas.peteraitis@gmail.com>
2022-02-04 18:57:02 +0200

    dep checking

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 
 use abi_stable::{
     sabi_types::{RMut, VersionStrings},
-    std_types::{RArc, RBox, RBoxError, RCow, RResult, RStr, RString},
+    std_types::{RArc, RBox, RBoxError, RCow, RResult, RSlice, RStr, RString, RVec, Tuple2},
 };
 
-pub const ABI: u32 = 13;
+pub const ABI: u32 = 14;
 
 #[repr(C)]
 pub struct BwsPlugin {
     pub name: RStr<'static>,
     pub version: RStr<'static>,
+    pub dependencies: RSlice<'static, Tuple2<RStr<'static>, RStr<'static>>>,
 }
