dc09dff8032e4328033c0518e196bca0883552ab
PonasKovas <mykolas.peteraitis@gmail.com>
2022-02-05 22:09:10 +0200

    tcplistener simple

--- bws/src/main.rs
+++ bws/src/main.rs
@@ -2,6 +2,7 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(unused_imports)]
 
+mod handle_connection;
 mod linear_search;
 mod opt;
 mod plugins;
@@ -12,9 +13,9 @@ use abi_stable::external_types::RRwLock;
 use abi_stable::std_types::{RArc, RString, RVec, Tuple2};
 use anyhow::{bail, Context, Result};
 use bws_plugin_interface::global_state::plugin::PluginList;
-use bws_plugin_interface::global_state::GlobalState;
+use bws_plugin_interface::global_state::{GState, GlobalState};
 pub use linear_search::LinearSearch;
-use log::{debug, error, info, warn};
+use log::{debug, error, info, trace, warn};
 use once_cell::sync::{Lazy, OnceCell};
 pub use shutdown::{shutdown, shutdown_started, wait_for_shutdown};
 use std::future::pending;
@@ -22,7 +23,10 @@ use std::sync::atomic::Ordering;
 use std::sync::mpsc::Receiver;
 use std::sync::Mutex;
 use std::{sync::atomic::AtomicU32, time::Duration};
+use tokio::net::TcpListener;
 use tokio::sync::{broadcast, mpsc};
+
+const NET_THREADS: usize = 4;
 
 fn main() -> Result<()> {
     let opt = opt::collect_opt();
@@ -38,7 +42,17 @@ fn main() -> Result<()> {
         .init();
 
     // Start the tokio runtime, for handling network IO
-    let rt = tokio::runtime::Runtime::new().context("Couldn't start tokio runtime")?;
+    let rt = if NET_THREADS == 1 {
+        tokio::runtime::Builder::new_current_thread()
+    } else {
+        let mut temp = tokio::runtime::Builder::new_multi_thread();
+        temp.worker_threads(NET_THREADS);
+        temp
+    }
+    .thread_name("bws-net-worker")
+    .enable_all()
+    .build()
+    .context("Couldn't start tokio runtime")?;
 
     // When any task panics, exit the whole app
     std::panic::set_hook(Box::new(move |info| {
@@ -51,8 +65,8 @@ fn main() -> Result<()> {
     info!("Loading plugins...");
     let plugins = plugins::load_plugins().context("Error loading plugins")?;
 
-    // Construct the global state, which may be needed when starting plugins already
-    let global_state = RArc::new(GlobalState {
+    // Construct the global state, which may already be needed when starting plugins
+    let gstate = RArc::new(GlobalState {
         plugins: RRwLock::new(PluginList(
             plugins
                 .into_iter()
@@ -62,12 +76,11 @@ fn main() -> Result<()> {
         vtable: vtable::VTABLE,
     });
 
-    plugins::start_plugins(&global_state).context("Couldn't start plugins")?;
+    plugins::start_plugins(&gstate).context("Couldn't start plugins")?;
 
     rt.block_on(async move {
-        tokio::spawn(net());
-
         tokio::select! {
+            _ = net(&gstate) => {}
             _ = wait_for_shutdown() => {},
             _ = tokio::signal::ctrl_c() => {},
             // On Unixes, handle SIGTERM too
@@ -92,13 +105,19 @@ fn main() -> Result<()> {
     })
 }
 
-async fn net() -> Result<()> {
-    tokio::select! {
-        _ = wait_for_shutdown() => {},
-        _ = async move {
-            // do work here
-        } => {},
-    }
+async fn net(gstate: &GState) -> Result<()> {
+    // Spawn a TCP listener
+    let listener = TcpListener::bind("[::]:22122").await?;
 
-    Ok(())
+    loop {
+        let conn = listener.accept().await?;
+
+        let gstate_clone = RArc::clone(gstate);
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection::handle_connection(gstate_clone, conn).await {
+                debug!("Connection handler error: {}", e);
+            }
+        });
+    }
 }
